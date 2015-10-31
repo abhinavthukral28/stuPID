3d73937c42d6367bacfe4a63b67fa2f78681ccec
Allan Luke <allanjluke@gmail.com>
2015-10-31 19:29:53 -0400

    QSql plz better errors

--- Database.cpp
+++ Database.cpp
@@ -8,20 +8,20 @@ const QString Database::DBpath="./testdatabase.db";
 Database* Database::instance=0;
 
 Database::Database(){
-   if(init())
-   {
-       qDebug("Something bad happened");
-   }
-   else {
-       qDebug("It worked!");
-   }
+    if(init())
+    {
+        qDebug("Something bad happened");
+    }
+    else {
+        qDebug("It worked!");
+    }
 
 
 }
 
 Database* Database::getInstance(){
     if(!Database::instance) {
-       Database::instance = new Database();
+        Database::instance = new Database();
     }
     return Database::instance;
 
@@ -34,19 +34,19 @@ int Database::init(){
 
     QSqlDatabase db = QSqlDatabase::addDatabase( "QSQLITE" );
 
-     db.setDatabaseName(Database::DBpath);
+    db.setDatabaseName(Database::DBpath);
 
-     if( !db.open() )
-     {
-       qDebug() << db.lastError();
-       qFatal( "Failed to connect." );
-     }
+    if( !db.open() )
+    {
+        qDebug() << db.lastError();
+        qFatal( "Failed to connect." );
+    }
 
-     qDebug( "Connected!" );
+    qDebug( "Connected!" );
 
 
-     createTables();
-     return 0;
+    createTables();
+    return 0;
 
 
 }
@@ -62,49 +62,49 @@ void Database::createTables(){
 
     qry.prepare( "CREATE TABLE IF NOT EXISTS Students (studentID INTEGER UNIQUE PRIMARY KEY AUTOINCREMENT, studentName VARCHAR(30))" );
     if( !qry.exec() )
-      qDebug() << qry.lastError();
+        qDebug() << qry.lastError();
     else
-      qDebug() << "Table Students created!";
+        qDebug() << "Table Students created!";
 
 
 
     qry.prepare( "CREATE TABLE IF NOT EXISTS Qualifications(qID INTEGER UNIQUE PRIMARY KEY AUTOINCREMENT, qualificationValue VARCHAR(30))" );
     if( !qry.exec() )
-      qDebug() << qry.lastError();
+        qDebug() << qry.lastError();
     else
-      qDebug() << "Table Qualifications created!";
+        qDebug() << "Table Qualifications created!";
 
     qry.prepare( "CREATE TABLE IF NOT EXISTS Projects(projectID INTEGER UNIQUE PRIMARY KEY AUTOINCREMENT, admin VARCHAR(30),projectName VARCHAR(30), description VARCHAR(30),minTeamSize int, maxTeamSize int)" );
     if( !qry.exec() )
-      qDebug() << qry.lastError();
+        qDebug() << qry.lastError();
     else
-      qDebug() << "Table Project created!";
+        qDebug() << "Table Project created!";
 
 
     qry.prepare( "CREATE TABLE IF NOT EXISTS teamMember(teamID INTEGER UNIQUE PRIMARY KEY AUTOINCREMENT,student_ID int,FOREIGN KEY(student_ID) REFERENCES Students(studentId))" );
     if( !qry.exec() )
-      qDebug() << qry.lastError();
+        qDebug() << qry.lastError();
     else
-      qDebug() << "Table teamMember created!";
+        qDebug() << "Table teamMember created!";
 
 
     qry.prepare( "CREATE TABLE IF NOT EXISTS Expectations(eID INTEGER UNIQUE PRIMARY KEY AUTOINCREMENT, expectationValue VARCHAR(30))" );
     if( !qry.exec() )
-      qDebug() << qry.lastError();
+        qDebug() << qry.lastError();
     else
-      qDebug() << "Table Expectations created!";
+        qDebug() << "Table Expectations created!";
 
     qry.prepare( "CREATE TABLE IF NOT EXISTS StudentQualifications(studentQualificationID INTEGER UNIQUE PRIMARY KEY AUTOINCREMENT, ratings VARCHAR(30), student_ID int, e_ID int,FOREIGN KEY(e_ID) REFERENCES Expectations(eId))" );
     if( !qry.exec() )
-      qDebug() << qry.lastError();
+        qDebug() << qry.lastError();
     else
-      qDebug() << "Table StudentQualifications created!";
+        qDebug() << "Table StudentQualifications created!";
 
     qry.prepare( "CREATE TABLE IF NOT EXISTS Team(teamID INTEGER UNIQUE PRIMARY KEY AUTOINCREMENT,project_ID int, FOREIGN KEY(project_ID) REFERENCES Projects(projectID))" );
     if( !qry.exec() )
-      qDebug() << qry.lastError();
+        qDebug() << qry.lastError();
     else
-      qDebug() << "Table Teams created!";
+        qDebug() << "Table Teams created!";
 
 }
 
@@ -113,19 +113,21 @@ QList<Student*>* Database::getAllStudents(){
 
     QSqlQuery query;
     query.prepare(DatabaseQueries::selectAllStudents);
-    qDebug() << DatabaseQueries::selectAllStudents;
+
     if (!query.exec())
     {
-      return NULL;
+        qDebug() << query.lastError();
+        qDebug() << query.lastQuery();
+        return NULL;
     }
     else
     {
         QList<Student*>* students = new QList<Student*>;
         while (query.next())
         {
-        QString username = QString(query.value(0).toString());
-        Student* temp = new Student(username);
-        students->append(temp);
+            QString username = QString(query.value(0).toString());
+            Student* temp = new Student(username);
+            students->append(temp);
         }
 
         return students;
@@ -138,8 +140,15 @@ QList<Student*>* Database::getAllStudents(){
 int Database::createStudent (Student& student){
     QSqlQuery query;
     query.prepare(DatabaseQueries::insertStudent);
-    query.addBindValue(student.getUsername());
-    return !query.exec() ? 1 : 0;
-
+    query.bindValue(":username",student.getUsername());
+    if(!query.exec())
+    {
+        qDebug() << query.lastError();
+        qDebug() <<query.lastQuery();
+        return 0;
+    }
+    else {
+        return query.lastInsertId().toInt();
+    }
 
 }
