ccc56c0939b793a8079aad92f51896e767886144
Allan Luke <allanjluke@gmail.com>
2015-11-02 05:47:36 -0500

    merging

--- main.cpp
+++ main.cpp
@@ -6,12 +6,47 @@
 #include "student.h"
 #include "studentprojectview.h"
 #include "editqualificationsview.h"
+#include "project.h"
 
-int main(int argc, char *argv[])
+
+int main()
 {
 
-    QApplication a(argc, argv);
+   // QApplication a(argc, argv);
+    Database* database = Database::getInstance();
 
-    return a.exec();
+    int projectID;
+    QString projectTitle = "ImAProject";
+    QString username = "ImAStudent";
+    QList<Student*> registeredStudents;
+    int ID = -1;
+    Project* project = new Project(ID,projectTitle);
+    Student* student = new Student (ID,username);
+    database->createStudent(*student);
+
+
+    project ->registerStudent(*student);
+    projectID = database->createProject(*project);
+
+
+
+
+    //database->addStudentToProject(projectID,*student);
+
+     std::cout<< "FETCHING" << std::endl;
+    QList<Project*>* projects = database->getAllProjects();
+    std::cout << projects->count() << std::endl;
+    for (int i = 0; i < projects->count(); i++)
+    {
+        project = projects->at(i);
+        std::cout<< project->getTitle().toStdString() << std::endl;
+        registeredStudents = project->getRegisteredStudents();
+        for (int j = 0; j < registeredStudents.count(); j++)
+        {
+            std::cout<< registeredStudents.at(j)->getUsername().toStdString();
+        }
+
+    }
+   // return a.exec();
 
 }
