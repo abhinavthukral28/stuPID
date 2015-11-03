1c6d149da272c909f3efd9df876cde97c876fc69
vinishadsouza <vinisha.n.dsouza@gmail.com>
2015-11-02 19:59:31 -0500

    wip

--- main.cpp
+++ main.cpp
@@ -1,53 +1,49 @@
 #include <QApplication> 
 #include <iostream>
-#include "logindialog.h"
 #include "Database.h"
-#include "user.h"
-#include "student.h"
-#include "studentprojectview.h"
-#include "editqualificationsview.h"
-#include "project.h"
-
+#include "manageprojectsview.h"
 
 int main(int argc, char *argv[])
 {
 
     QApplication a(argc, argv);
-    Database* database = Database::getInstance();
+    ManageProjectsView view;
+    view.show();
+//    Database* database = Database::getInstance();
 
-    int projectID;
-    QString projectTitle = "ImAProject";
-    QString username = "ImAStudent";
-    QList<Student*> registeredStudents;
-    int ID = -1;
-    Project* project = new Project(ID,projectTitle);
-    Student* student = new Student (ID,username);
-    database->createStudent(*student);
-
-
-    project ->registerStudent(*student);
-    projectID = database->createProject(*project);
+//    int projectID;
+//    QString projectTitle = "ImAProject";
+//    QString username = "ImAStudent";
+//    QList<Student*> registeredStudents;
+//    int ID = -1;
+//    Project* project = new Project(ID,projectTitle);
+//    Student* student = new Student (ID,username);
+//    database->createStudent(*student);
 
 
+//    project ->registerStudent(*student);
+//    projectID = database->createProject(*project);
 
 
-    //database->addStudentToProject(projectID,*student);
-
-     std::cout<< "FETCHING" << std::endl;
-    QList<Project*>* projects = database->getAllProjects();
-    std::cout << projects->count() << std::endl;
-    for (int i = 0; i < projects->count(); i++)
-    {
-        project = projects->at(i);
-        std::cout<< project->getTitle().toStdString() << std::endl;
-        registeredStudents = project->getRegisteredStudents();
-        for (int j = 0; j < registeredStudents.count(); j++)
-        {
-            std::cout<< registeredStudents.at(j)->getUsername().toStdString();
-        }
 
 
-    }
+//    //database->addStudentToProject(projectID,*student);
+
+//     std::cout<< "FETCHING" << std::endl;
+//    QList<Project*>* projects = database->getAllProjects();
+//    std::cout << projects->count() << std::endl;
+//    for (int i = 0; i < projects->count(); i++)
+//    {
+//        project = projects->at(i);
+//        std::cout<< project->getTitle().toStdString() << std::endl;
+//        registeredStudents = project->getRegisteredStudents();
+//        for (int j = 0; j < registeredStudents.count(); j++)
+//        {
+//            std::cout<< registeredStudents.at(j)->getUsername().toStdString();
+//        }
+
+
+//    }
   return a.exec();
 
 
