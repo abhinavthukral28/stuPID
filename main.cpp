995588e7d61f03b882f19866d690d35210f87fdc
Abhinav Thukral <abhinav@abhinavthukral.com>
2015-11-04 06:29:23 -0500

    wip qualification init

--- main.cpp
+++ main.cpp
@@ -9,27 +9,10 @@ int main(int argc, char *argv[])
 {
 
     QApplication a(argc, argv);
-    ManageProjectsView view;
+    LoginDialog view;
     view.show();
-//    LoginDialog view;
-//    view.show();
 
 
-    Database* database = Database::getInstance();
-    int tempID = -1;
-    QString title = "imaproject";
-    Project* project = new Project(tempID,title);
-
-    database->createProject(*project);
-
-    QList<Project*>* projects = database->getAllProjects();
-
-    for (int i = 0; i < projects->count(); i++)
-    {
-        qDebug() << "this is fucked";
-        QString temp = projects->at(i)->getTitle();
-        std::cout << QString::toStdString(temp);
-    }
 
   return a.exec();
 
--- qualificationinit.cpp
+++ qualificationinit.cpp
@@ -2,6 +2,7 @@
 #include "ui_qualificationinit.h"
 #include "qualificationinitController.h"
 #include "student.h"
+#include <QDebug>
 QualificationInit::QualificationInit(Student &student) :
     QDialog(),
     ui(new Ui::qualificationinit),
@@ -11,10 +12,23 @@ QualificationInit::QualificationInit(Student &student) :
 
 {
     ui->setupUi(this);
-    ui->NameLabel->setText("Git");
-    ui->NumberLabel->setText("1/20");
-    ui->QuesTopLabel->setText("Do you even Git bro?");
-    ui->QuesDownLabel->setText("How much can the other do?");
+    keyList.append("Git");
+    keyList.append("OO");
+    keyList.append("GPA");
+    keyList.append("WD");
+
+    e.append("How much your partner gits");
+    q.append("Do you do object oriented pogramming?");
+    e.append("How much your partner knows");
+    q.append("What's you CGPA");
+    e.append("Parner's expected CGPA");
+    q.append("Do you know web dev");
+    e.append("How much does your partner know");
+    activeIndex = 0;
+    ui->keyLabel->setText(keyList.at(0));
+    ui->NumberLabel->setText("1");
+    ui->QuesTopLabel->setText(q.at(0));
+    ui->QuesDownLabel->setText(e.at(0));
 }
 
 QualificationInit::~QualificationInit()
@@ -24,3 +38,20 @@ QualificationInit::~QualificationInit()
 
 
 
+
+void QualificationInit::on_NextButton_clicked()
+{
+
+    activeIndex += 1;
+    if(activeIndex >= keyList.length()){
+
+    }
+    else{
+        qDebug() << activeIndex;
+
+        ui->keyLabel->setText(keyList.at(activeIndex));
+        ui->NumberLabel->setText(QString::number(activeIndex));
+        ui->QuesTopLabel->setText(q.at(activeIndex));
+        ui->QuesDownLabel->setText(e.at(activeIndex));
+    }
+}
