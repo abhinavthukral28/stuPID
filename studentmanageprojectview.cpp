42830f36293a82a6bd0243689b2f0d988dfe3fe0
Abhinav Thukral <abhinav@abhinavthukral.com>
2015-11-24 17:01:21 -0500

    recoved files

--- studentmanageprojectview.cpp
+++ studentmanageprojectview.cpp
@@ -12,21 +12,3 @@ StudentManageProjectView::~StudentManageProjectView()
 {
     delete ui;
 }
-
-void StudentManageProjectView::on_listView_doubleClicked(const QModelIndex &index)
-{
-    //int index = index.row();
-
-   //studentmanageprojectviewcontroller->updateSelectedProject(index);
-}
-
-int StudentManageProjectView::updateProjectsList(QList<QString> &projectTitles){
-     QString temp;
-     for (int i = 0; i <projectTitles.count();i++)
-     {
-         temp =projectTitles.at(i);
-        ui->projectsList->addItem(temp);
-     }
-    // manageProjectsController->updateSelectedProject(0);
-
- }
