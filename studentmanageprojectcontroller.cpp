d0001b34d870b76b8c489c366029183e2ca506b8
vinishadsouza <vinisha.n.dsouza@gmail.com>
2015-11-26 15:42:47 -0500

    ehhh not sure what im doing

---
+++ studentmanageprojectcontroller.cpp
@@ -0,0 +1,5 @@
+#include "studentmanageprojectcontroller.h"
+
+studentmanageprojectcontroller::studentmanageprojectcontroller()
+{
+}
---
+++ studentmanageprojectcontroller.h
@@ -0,0 +1,10 @@
+#ifndef STUDENTMANAGEPROJECTCONTROLLER_H
+#define STUDENTMANAGEPROJECTCONTROLLER_H
+
+class studentmanageprojectcontroller
+{
+public:
+    studentmanageprojectcontroller();
+};
+
+#endif // STUDENTMANAGEPROJECTCONTROLLER_H
