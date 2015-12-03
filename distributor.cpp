97099235dab0ddf4765fe7cfdd05d0d5658e70b5
Abhinav Thukral <abhinav@abhinavthukral.com>
2015-12-02 20:20:32 -0500

    Added Distributor

--- distributor.cpp
+++ distributor.cpp
@@ -1,34 +1,37 @@
 #include "distributor.h"
 #include "team.h"
+#include "student.h"
 
 Distributor::Distributor(const QMap< int,QList< QPair<int,int> > >& pciParam) : pci(pciParam)
 {
 
 }
 
- const QList<Team*>& Distributor::distributeTeams(const int minSize,const int maxSize){
-     QList <Team*>* teams = new QList<Team*>;
+const QList<Team*>& Distributor::distributeTeams(const int minSize,const int maxSize){
+    QList <Team*>* teams = new QList<Team*>;
 
 
-     int numTeams = pci.keys().count()/minSize;
+    int numTeams = pci.keys().count()/minSize;
 
-     for (int i = 0; i < numTeams; i++){
-         //QPair<int,int> pair = pci.value(pci.keys.at(i));
-     }
+    for (int i = 0; i < numTeams; i++){
+        //QPair<int,int> pair = pci.value(pci.keys.at(i));
+    }
 
-     return *teams;
- }
+    return *teams;
+}
 
 
 int Distributor::calculateTeamWeight(Team team)
 {
-    QList<int> total;
-    QList<Student*> teamMembers = team.getTeamMembers();
-        for(int i=0;i<teamMembers.count();i++){
-        int a = 0;
-        }
-        //total =(team.getTeamMembers().at(i));
- return -1;
+    QList<int> pciValues;
+    QList<int> teamMembers = team.getTeamMembers();
+
+    for(int i=0; i<teamMembers.count();i++){
+        teamMembers.at(i);
+
+    }
+    //total =(team.getTeamMembers().at(i));
+    return -1;
 }
 
 int Distributor::assignStudentToTeam(int studentID, QList<Team*> teams)
