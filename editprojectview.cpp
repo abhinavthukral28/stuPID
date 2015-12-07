a265e02673249874acd762ab7a8020e9e563eb96
vinishadsouza <vinisha.n.dsouza@gmail.com>
2015-12-07 18:50:00 -0500

    EditProject working

--- editprojectview.cpp
+++ editprojectview.cpp
@@ -1,10 +1,13 @@
 #include "editprojectview.h"
 #include "ui_editprojectview.h"
+#include "editprojectcontroller.h"
 
 EditProjectView::EditProjectView(QWidget *parent) :
     QDialog(parent),
-    ui(new Ui::EditProjectView)
+    ui(new Ui::EditProjectView),
+    editProjectController(EditProjectController::getInstance())
 {
+    editProjectController->setView(this);
     ui->setupUi(this);
 }
 
@@ -17,3 +20,45 @@ void EditProjectView::on_buttonBox_accepted()
 {
 
 }
+
+
+const QString EditProjectView::getProjectDescription()
+ {
+     aDescription = ui->projectDescriptionTextFeild->toPlainText();
+     QString desc = ui->projectDescriptionTextFeild->toPlainText();
+     return desc;
+
+ }
+
+const QString EditProjectView::getProjectTitle(){
+    aTitle = ui->projectNameTextFeild->text();
+    //vinisha
+    QString title =ui->projectNameTextFeild->text();
+    return title;
+}
+
+const int& EditProjectView::getProjectTeamMin(){
+
+    aMin = ui->minTeamSizeTextFeild->text();
+    int b =aMin.toInt();
+    int& ref3=b;
+    return ref3;
+
+}
+
+
+
+const int& EditProjectView::getProjectTeamMax()
+{
+    aMax = ui->maxTeamSizeTextFeild->text();
+    int x = aMax.toInt();
+    int &ref4=x;
+    return ref4;
+}
+
+void EditProjectView::accept(){
+
+}
+    ;
+
+
--- editprojectview.h
+++ editprojectview.h
@@ -2,17 +2,25 @@
 #define EDITPROJECTVIEW_H
 
 #include <QDialog>
-
+#include "editprojectcontroller.h"
 namespace Ui {
 class EditProjectView;
 }
 
+//class EditProjectController;
 class EditProjectView : public QDialog
 {
     Q_OBJECT
 
 public:
     explicit EditProjectView(QWidget *parent = 0);
+    const QString getProjectDescription();
+    const QString getProjectTitle();
+    const int& getProjectTeamMin();
+    const int& getProjectTeamMax();
+
+    QString aTitle, aMin, aMax, aDescription;
+    void accept();
     ~EditProjectView();
 
 private slots:
@@ -20,6 +28,7 @@ private slots:
 
 private:
     Ui::EditProjectView *ui;
+    EditProjectController *editProjectController;
 };
 
 #endif // EDITPROJECTVIEW_H
